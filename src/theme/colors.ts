7dfaf7582e7a2236b0fef182a44ffdcc2837ddcd
Nick Hansen <nitronick600@gmail.com>
2020-09-27 19:41:58 -0700

    Add magenta / cyan theming (#3)
    Co-authored-by: Nick Hansen <nick@luxor.tech>

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -2,21 +2,21 @@ export const white = '#FFF'
 export const black = '#000'
 
 export const green = {
-  500: '##00d1810',
+  500: '#00d1810',
 }
 
 export const red = {
-  100: '#FFFDFE',
-  200: '#ffc2a8',
-  500: '#d16c00',
+  100: '#5a3444',
+  200: '#b36888',
+  500: '#d1a4b8',
 }
 
 export const grey = {
-  100: '#f7f4f2',
-  200: '#f0e9e7',
-  300: '#e2d6cf',
-  400: '#aa9585',
-  500: '#805e49',
-  600: '#5b3926',
-  800: '#2d1e12',
+  100: '#f4fff5',
+  200: '#caffce',
+  300: '#a0ffa7',
+  400: '#77cc7e',
+  500: '#68b36e',
+  600: '#3c663f',
+  800: '#0f1910',
 }
